b311c915fa57bb187276cc3ba89ce7d4a2dfe737
Jason Zhao <30389316+jasmineve@users.noreply.github.com>
2017-07-28 19:19:45 +0800

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,18 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+print "What's your name?"
+name = raw_input()
+print "Where are you from?"
+city = raw_input()
+print "What's your favourite food?"
+answer = raw_input()
+
+print "This is truly astonishing for someone like %s, who's from %s and who enjoys %s." % (name, city, answer)
