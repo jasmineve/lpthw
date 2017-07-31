a705d45b1e0c2b01d188ed84422735085660efa7
Jason Zhao <30389316+jasmineve@users.noreply.github.com>
2017-07-31 15:48:04 +0800

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+name, sex, age = argv
+
+
+print "And your sex is:", sex
+print "You are", age, "years old."
+
+tax = raw_input('What is your name?')
+print "So, your name is:", tax
